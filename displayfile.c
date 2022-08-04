0811b489144ce301fecfa5a51b7d240dce54257c
GouriVarma <96877313+GouriVarma@users.noreply.github.com>
2022-08-04 20:13:30 +0530

    Create displayfile.c

---
+++ displayfile.c
@@ -0,0 +1,45 @@
+//Write a  C program to display files in a given directory
+
+#include <stdio.h>
+
+#include <dirent.h>
+
+int main()
+
+{
+
+    struct dirent *de;
+
+    char filepath [100];
+
+    printf("Enter the path of the directory:\n ");
+
+    scanf("%s",filepath);
+
+    DIR *dr=opendir(filepath);
+
+    if(dr==NULL)
+
+    {
+
+        printf("Could not open the directory");
+
+        return 0;
+
+    }
+
+    printf("Files in the directory are:\n");
+
+    while((de=readdir(dr))!=NULL)
+
+    {
+
+        printf("\n%s\n",de->d_name);
+
+    }
+
+    closedir(dr);
+
+    return 0;
+
+}
